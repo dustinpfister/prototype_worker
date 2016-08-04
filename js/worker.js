34a0c9319c82a268aacb311ca195af4b1c9fa9f9
Dustin John Pfister <dustin.pfister@gmail.com>
2016-08-04 13:08:14 -0400

    Worker default rate set to area.minWage, happy and greed multi are now random

--- js/worker.js
+++ js/worker.js
@@ -9,12 +9,12 @@ var Worker = (function(){
 
     Worker = function(area, rate, happyMulti, greedMulti){
 
-        this.rate = rate;
+        this.rate = area.minWage;
         this.happy = 1;
 
         // profile
-        this.happyMulti = happyMulti;    // how important being happy is to them
-        this.greedMulti = greedMulti;    // how important money is to them.
+        this.happyMulti = 1 + Number(  (Math.random() * 2).toFixed(2) );    // how important being happy is to them
+        this.greedMulti = 1 + Number(  (Math.random() * 2).toFixed(2) );    // how important money is to them.
 
         this.effort = 0;
         this.setEffort();
@@ -29,7 +29,7 @@ var Worker = (function(){
         var byPayRate = this.rate / area.livingWage * (this.greedMulti / this.happyMulti),
         byHappy = this.happy * this.happyMulti;
 
-        this.effort = byPayRate + byHappy;
+        this.effort = Number((byPayRate + byHappy).toFixed(2));
 
     };
 
