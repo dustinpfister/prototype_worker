ba0b5b006a454f98fc5075d256bb69c5daa73865
Dustin John Pfister <dustin.pfister@gmail.com>
2016-08-04 18:34:24 -0400

    userMain

--- js/control.js
+++ js/control.js
@@ -1,22 +1,50 @@
 var control = (function () {
 
-    var userMain = function(e){
-		
-		console.log(e);
-		
-	}
+    var userMain = function (e, done) {
 
+        var box = e.target.getBoundingClientRect(),
+        x,
+        y;
 
-	return {
-		
-		// attach events to the given canvas
-		attachToCanvas : function(canvas){
-			
-			canvas.addEventListener('mousedown', userMain);
-			
-		}
-		
-	}
-	
+        e.preventDefault();
+
+        if (e.touches) {
+
+            x = e.touches[0].clientX;
+            y = e.touches[0].clientY;
+
+        } else {
+
+            x = e.clientX;
+            y = e.clientY;
+
+        }
+
+        x = Math.floor(x - box.left);
+        y = Math.floor(y - box.top);
+
+        done(e, x, y, box);
+
+    };
+
+    return {
+
+        // attach events to the given canvas
+        attachToCanvas : function (canvas) {
+
+            canvas.addEventListener('mousedown', function (e) {
+
+                userMain(e, function (e, x, y, box) {
+
+                    console.log(y);
+
+                });
+
+            });
+
+        }
+
+    }
+
 }
     ());
